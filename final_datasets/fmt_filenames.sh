4d11ab26422e56c4f995dc1b2aa44e274a7604e3
Evan Chow <e.chow1@lse.ac.uk>
2021-05-01 17:35:56 +0100

    Working on kapustin piece with tagging

--- final_datasets/fmt_filenames.sh
+++ final_datasets/fmt_filenames.sh
@@ -1,4 +1,4 @@
-find . -type f -name 'Rach*' | while read FILE ; do
-    newfile="$(echo ${FILE} |sed -e 's/Rachmaninov/rachmaninoff/')" ;
+find . -type f -name 'kapustin*' | while read FILE ; do
+    newfile="$(echo ${FILE} |sed -e 's/PIANO_AND_BEAT/piano_PITCH/')" ;
     mv "${FILE}" "${newfile}" ;
 done
